67b0cf2bc218ebdd43a7aac016bb4586096e45c3
jb5595 <jacobberman1995@gmail.com>
2018-09-03 16:44:35 -0400

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "m", "brown","mean", 1 );
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "brown","m", "mean", 1 );
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "brown","F", "mean", 1 );
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, "brown","m", "mean", 1 );
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Melissa", 13, "brown","F", "mean", 0 );
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "brown","m", "mean", 0 );
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "brown","F", "mean", 0 );
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (NULL, 2, "brown","m", "mean", 0 );
