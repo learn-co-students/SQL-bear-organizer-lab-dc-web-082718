71cdf7076840a64336109e8968ea7ab3fcd2be42
Trevor Jameson <github email address>
2018-09-06 09:47:35 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("ice bear", 4, "male", "white", "cool", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("griz", 5, "male", "brown", "brotherly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("panda", 3, "male", "white & black", "sempai", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("smoky", 4, "female", "brown", "cool", 0);
+INSERT INTO bears ( age, gender, color, temperament, alive) VALUES ( 5, "male", "brown", "brotherly", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("dokey", 4, "female", "black", "cool", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("that bear from anchorman", 2, "female", "white", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("that bear i saw at the circus that one time", 2, "female", "polka dot", "sad", 0);
