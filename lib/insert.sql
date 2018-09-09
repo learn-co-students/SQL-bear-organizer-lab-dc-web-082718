49a9fe31cd12cc89b38791c4bcd4869c4911bba7
Srikant Kumar Kalaputapu <sriki@Srikants-MBP.home>
2018-09-09 09:10:09 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+insert into bears (name,age,gender,color,temperament,alive) VALUES ("Mr. Chocolate",21,"M","dark brown","angry",1);
+insert into bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",34,"M","black","angry",1);
+insert into bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",23,"F","dark brown","angry",1);
+insert into bears (name,age,gender,color,temperament,alive) VALUES ("Sergeant Brown",12,"M","Green","happy",0);
+insert into bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",14,"F","dark brown","sad",1);
+insert into bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",12,"M","Black","lonely",0);
+insert into bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",31,"F","Blue","mad",1);
+insert into bears (name,age,gender,color,temperament,alive) VALUES (null,41,"M","black","excited",0);
